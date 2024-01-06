b414a0333cbc21a467483696ada2cccea3181f68
alireza rais sattari <95269267+alirezaRaisSattari@users.noreply.github.com>
2024-01-06 15:15:17 +0330

    kill a port

--- bash.sh
+++ bash.sh
@@ -1,2 +1,5 @@
-//change patial name of file or directory name 
+// change patial name of file or directory name 
 find ./web -depth -type f -name '*Host*' -exec rename -v 's/Host/Platform/g' {} \;
+
+// kill port process
+sudo kill -9 $(sudo lsof -t -i:9001)
